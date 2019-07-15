baf0b789cfadc456f5ac518f4b27555dc62c8419
Jose Francisco Javier Gonzalez Pineda <fjgonzalez@crecer.com.sv>
2019-07-14 22:15:55 -0600

    my third commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,8 +17,7 @@ public class Greeter {
    * @param someone
    * @return
    */
-  
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
